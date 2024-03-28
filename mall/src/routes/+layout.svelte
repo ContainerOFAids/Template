552d1aa91b71aaad1cba8114144b981d56cca708
sam <samgus02@edu.umea.se>
2024-03-28 16:45:20 +0100

    salami

---
+++ mall/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+<div class="navbar flex flex-row bg-primary-500 p-3">
+	
+	<a href="https://github.com/ContainerOFAids/Mall" class=""><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" class=" aspect-square w-10 hover:bg-white rounded-full"></a>
+	
+	<div class=" justify-center flex flex-row gap-10 w-full">
+		<a href="http://localhost:5173/" class="navbtn h2 hover:text-gray-300">Home</a>
+		<a href="" class="navbtn h2 hover:text-gray-300">About Us</a>
+		<a href="" class="navbtn h2 hover:text-gray-300">Profile</a>
+	</div>
+	
+	<img src="" alt="">
+
+</div>
+<slot />
---
+++ mall/src/routes/register/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { enhance } from '$app/forms';
+    import type { ActionData, PageData } from './$types';
+    
+    export let data: PageData;
+    export let form: ActionData;
+    let s ="";
+</script>
+
+<main>
+    <div class="registerContainer">
+        <h1>Register</h1>
+        <div class="registerContainerForm">
+            <form action="?/register" method="post" use:enhance>
+                <div class="test">
+                    <label>Username: <input type="text" name="username" class="input"></label>
+                    <label>Password: <input type="password" name="password" class="input"></label>
+                    <label>Repeat Password: <input type="password" name="password-repeat" class="input"></label>
+                    <label>Profile Picture: <input type="text" placeholder="image url" bind:value={s} class="input"></label>
+                    <label><input type="hidden" value={s} name="pic"></label>
+                    <button>Log In</button>
+                    {#if form?.msg}
+                        <span>{form.msg}</span>
+                    {/if}
+                </div>
+            </form>
+        </div>
+        <a href="/login">Already have account?</a>
+    </div>
+</main>
+
+ 
