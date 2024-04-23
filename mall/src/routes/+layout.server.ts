1847f40c0ad73dda12bc3a72e359af66d21dda66
sam <samgus02@edu.umea.se>
2024-04-23 11:08:23 +0200

    salami

--- mall/src/routes/+layout.server.ts
+++ mall/src/routes/+layout.server.ts
@@ -1,15 +1,29 @@
-import { PrismaClient } from '@prisma/client';
-import type { LayoutServerLoad } from './$types';
-
+import {PrismaClient} from "@prisma/client";
+import type { LayoutServerLoad } from "./$types";
 const prisma = new PrismaClient();
 
-export const load = (async () => {
-    const token = await prisma.token.findMany({});
-    // let profilpic = await prisma.user.findUnique ({
-    //     where: {
-    //         pic: token.id
-    //     }
-    // })
-    let user = await prisma.user.findMany({})
-    return {token,user};
+export const load = (async ({cookies}) => {
+    let token = cookies.get("username") ?? "";
+    let prismaToken = await prisma.token.findUnique({
+        where: { id: token },
+        include: { user: true }
+    });
+    
+    if(!prismaToken) return {isAdmin: false}
+    let user = prismaToken.user;
+    let admin = user.isAdmin
+
+    
+    
+    return {isAdmin: admin, user};
 }) satisfies LayoutServerLoad;
+
+export const _findCurrentUser = async (token : string) => {
+    let prismaToken = await prisma.token.findUnique({
+    where: { id: token },
+    include: { user: { select: { name: true, id: true } } }
+});
+if(!prismaToken) return {id: "", name: ""};
+
+return prismaToken.user;
+};
--- mall/src/routes/+layout.svelte
+++ mall/src/routes/+layout.svelte
@@ -12,18 +12,32 @@
 
 	
 </script>
+
 <div class="appbar flex flex-row bg-surface-100-800-token p-3 mb-10 shadow-2xl">
 	
 	<a href="https://github.com/ContainerOFAids/Mall" class="btn-icon text-black hover:text-white"><i class="fa-brands fa-github fa-4x ml-5"></i></a>
 	
 	<div class=" justify-center flex flex-row gap-10 w-full">
-		<a href="http://localhost:5173/" class="btn h2 font-bold hover:variant-soft-primary">Home</a>
-		<a href="" class="btn h2 font-bold hover:variant-soft-primary">About Us</a>
-		<a href="" class="btn h2 font-bold hover:variant-soft-primary">Profile</a>
-
+		<a href="/" class="btn h2 font-bold hover:variant-soft-primary">Home</a>
+		<a href="/activities" class="btn h2 font-bold hover:variant-soft-primary">Activities</a>
+		<a href="/login" class="btn h2 font-bold hover:variant-soft-primary">Log In</a>
+		
+		
+		{#if data.isAdmin}
+			<a href="/admin-panel" class="btn h2 font-bold hover:variant-soft-primary">Admin panel</a>
+		{/if}
+		<a href="/profile" class=" absolute right-0">
+			<div class="flex mr-2">
+				<h2 class="h2 flex items-center">{data.user?.name}</h2>
+				<img class="avatar-image w-16 rounded-full aspect-square ml-2" src="{data.user?.pic}" alt="">
+			</div>
+		</a>
 	</div>
-	
-	<img src="" alt="" class="profilepic aspect-square">
 
 </div>
 <slot />
+
+<div class=" bg-surface-900 h-56 pt-10 pl-10 mt-10 absolute w-full">
+	<header>Epost:</header>
+	<h2 class="h3">Samgus02@edu.umea.se</h2>
+</div>
